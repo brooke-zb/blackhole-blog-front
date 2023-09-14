4199a1c7464bffa83d3c5d95b2dfcbd405193064
brooke_zb <brooke_zb@qq.com>
2023-09-15 01:01:11 +0800

    feat: add menu component

---
+++ src/components/bh-menu/bh-menu-item.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="p-2 transition-colors hover:bg-primary-200 dark:hover:bg-dark-600 cursor-pointer">
+    <slot />
+  </div>
+</template>
---
+++ src/components/bh-menu/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <!-- arrow -->
+  <div class="relative group/menu cursor-pointer">
+    <slot />
+    <div
+      class="absolute top-full left-0 text-sm bg-gray-50 dark:bg-slate-800 shadow py-1
+      border dark:border-slate-700 rounded invisible group-hover/menu:visible z-10
+      scale-y-50 group-hover/menu:scale-y-100 opacity-0 group-hover/menu:opacity-100
+      transition-all origin-top delay-150 text-gray-600 dark:text-gray-200 fill-gray-600 dark:fill-gray-200"
+    >
+      <slot name="menu" />
+    </div>
+  </div>
+</template>
