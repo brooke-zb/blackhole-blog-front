3b35eb465a27d38ab15fa3c378ade496d0b2ac87
brooke_zb <brooke_zb@qq.com>
2023-09-17 14:36:06 +0800

    feat: add admin article api

---
+++ src/api/admin/article.ts
@@ -0,0 +1,42 @@
+import { request } from '@/utils'
+
+export default {
+  getByAid(aid: number | string) {
+    return request.get<BhArticle>(`/admin/articles/${aid}`)
+  },
+
+  getList(page = 1, condition: BhArticleCondition = {}) {
+    return request.get<Page<BhArticlePreview>>('/articles', {
+      params: { page, ...condition },
+    })
+  },
+
+  add(article: BhArticle) {
+    return request.post<void>('/admin/articles', {
+      data: article,
+    })
+  },
+
+  addAttachment(attachment: File) {
+    // FormData
+    const formData = new FormData()
+    formData.append('file', attachment)
+
+    return request.post<void>('/admin/articles/attachments', {
+      data: formData,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+  },
+
+  update(article: BhArticleAdd) {
+    return request.put<void>('/admin/articles', {
+      data: article,
+    })
+  },
+
+  delete(aid: number | string) {
+    return request.delete<void>(`/admin/articles/${aid}`)
+  },
+}
--- src/api/admin/index.ts
+++ src/api/admin/index.ts
@@ -1,1 +1,5 @@
-export default {}
+import article from './article'
+
+export default {
+  article,
+}
