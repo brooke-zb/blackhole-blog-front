98117db41bfe2c1679450c5c41c5f943a3ee69ea
brooke_zb <brooke_zb@qq.com>
2023-09-17 14:35:48 +0800

    feat: add table component

---
+++ src/components/bh-table/index.vue
@@ -0,0 +1,107 @@
+<script setup lang="ts" generic="T extends Record<string, unknown>">
+const props = defineProps<{
+  data: T[]
+  cols?: (keyof T)[]
+}>()
+
+const renderHeader = computed(() => {
+  if (!props.cols) {
+    return props.data.keys()
+  }
+  return props.cols
+})
+
+const renderData = computed(() => {
+  return props.data.map((item) => {
+    if (!props.cols) {
+      return item
+    }
+    const data: T = {} as T
+    props.cols.forEach((col) => {
+      data[col] = item[col]
+    })
+    return data
+  })
+})
+</script>
+
+<template>
+  <!-- eslint-disable vue/require-v-for-key -->
+  <div class="overflow-x-auto">
+    <table class="table-auto w-full bg-gray-50 dark:bg-slate-800">
+      <!-- header -->
+      <thead>
+        <tr>
+          <slot name="header">
+            <th v-for="header in renderHeader" :key="header">
+              {{ header }}
+            </th>
+          </slot>
+        </tr>
+      </thead>
+
+      <!-- body -->
+      <tbody>
+        <tr v-for="item in renderData">
+          <slot :row-data="item">
+            <td v-for="col in item">
+              {{ col }}
+            </td>
+          </slot>
+        </tr>
+      </tbody>
+
+      <!-- empty -->
+      <caption v-if="props.data.length === 0" class="caption-bottom">
+        <div class="py-8">
+          <i-regular-box-open class="w-12 h-12 fill-gray-500 dark:fill-gray-400" />
+          <p class="text-gray-500 dark:text-gray-400">
+            暂无数据
+          </p>
+        </div>
+      </caption>
+
+      <!-- footer -->
+      <caption class="caption-bottom">
+        <slot name="footer" />
+      </caption>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+tr {
+  @apply border-b border-gray-300 dark:border-gray-600;
+}
+
+tr th,
+tr td,
+tr :slotted(th),
+tr :slotted(td) {
+  @apply bg-gray-50 text-start p-2 whitespace-nowrap;
+}
+.dark tr th,
+.dark tr td,
+.dark tr :slotted(th),
+.dark tr :slotted(td) {
+  @apply bg-slate-800;
+}
+
+tbody tr:nth-child(odd) td,
+tbody tr:nth-child(odd) :slotted(td) {
+  @apply bg-primary-100;
+}
+.dark tbody tr:nth-child(odd) td,
+.dark tbody tr:nth-child(odd) :slotted(td) {
+  @apply bg-slate-700;
+}
+
+tbody tr:hover td,
+tbody tr:hover :slotted(td) {
+  @apply bg-primary-200;
+}
+.dark tbody tr:hover td,
+.dark tbody tr:hover :slotted(td) {
+  @apply bg-slate-600;
+}
+</style>
