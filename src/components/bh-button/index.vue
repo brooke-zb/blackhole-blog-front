2a313ccf97c8d59d35cfa784eaf83f39ea6b07b7
brooke_zb <brooke_zb@qq.com>
2023-09-20 16:27:16 +0800

    feat: add disabled prop for button component

--- src/components/bh-button/index.vue
+++ src/components/bh-button/index.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 const props = defineProps<{
   noRing?: boolean
+  disabled?: boolean
 }>()
 
 const ringClass = 'button-ring hover:ring-2 focus:ring-2 ring-offset-2 dark:ring-offset-slate-800 after:absolute after:transition-opacity after:inset-0 after:bg-black after:rounded-md after:opacity-0 active:after:opacity-30 outline-0'
+const disabledClass = 'opacity-50 !cursor-not-allowed'
 </script>
 
 <template>
   <button
     class="select-none cursor-pointer p-1 relative inline-flex justify-center items-center rounded-md transition-shadow"
-    :class="[props.noRing ? '' : ringClass]"
+    :class="[props.noRing || props.disabled ? '' : ringClass, props.disabled ? disabledClass : '']"
+    :disabled="props.disabled"
   >
     <slot name="icon" />
     <div v-if="$slots.default" :class="{ 'ml-1': $slots.icon && $slots.default }">
