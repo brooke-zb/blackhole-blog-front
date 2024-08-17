6b46200e4240141c01acafdfd30fc9ab0bd6e909
brooke_zb <brooke_zb@qq.com>
2024-08-18 00:25:39 +0800

    feat: navbar链接区适配小屏幕

--- src/layouts/components/nav-bar/index.vue
+++ src/layouts/components/nav-bar/index.vue
@@ -16,15 +16,17 @@ const links = [
   { name: t('routes.about'), path: '/about', icon: IconRegularAddressCard },
   { name: t('routes.friend'), path: '/friends', icon: IconRegularFaceSunglasses },
 ]
+const isLinksOpen = ref(false)
+const linkMenuHeight = ref(links.length) // * 3rem
 const navTitle = import.meta.env.BHBLOG_APP_TITLE
 </script>
 
 <template>
   <nav
-    class="sticky top-0 h-14 px-2 py-1 flex justify-between items-center z-30
-    ring-1 ring-gray-500/10 dark:ring-gray-500/10 bg-gray-50 dark:bg-slate-900 bg-opacity-60 backdrop-blur"
+    class="sticky top-0 px-2 py-1 flex justify-between items-center z-30
+    ring-1 ring-gray-500/10 dark:ring-gray-500/10 bg-gray-50 dark:bg-slate-900 bg-opacity-60 backdrop-blur flex-wrap md:flex-nowrap"
   >
-    <div class="flex gap-2 ps-1.5 items-baseline grow">
+    <div class="flex gap-2 ps-1.5 items-center grow h-12">
       <router-link to="/" class="text-lg">
         {{ navTitle }}
       </router-link>
@@ -32,15 +34,16 @@ const navTitle = import.meta.env.BHBLOG_APP_TITLE
         {{ titleStore.title }}
       </div>
     </div>
+
+    <!-- 链接区 -->
     <div class="flex items-center gap-5 sm:gap-4">
       <router-link
         v-for="link in links" :key="link.path" :to="link.path"
         class="text-gray-600 hover:text-gray-950 dark:text-gray-400 dark:hover:text-gray-100
         fill-gray-600 hover:fill-gray-950 dark:fill-gray-400 dark:hover:fill-gray-100
-        transition-colors duration-200"
+        hidden sm:inline transition-colors duration-200"
       >
-        <span class="hidden sm:inline">{{ link.name }}</span>
-        <component :is="link.icon" v-tooltip="link.name" class="sm:hidden" />
+        {{ link.name }}
       </router-link>
       <bh-button
         v-tooltip="t('nav.swtich-theme')"
@@ -68,6 +71,42 @@ const navTitle = import.meta.env.BHBLOG_APP_TITLE
           </template>
         </bh-button>
       </router-link>
+      <bh-button
+        class="sm:hidden text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100
+        fill-gray-600 hover:fill-gray-900 dark:fill-gray-400 dark:hover:fill-gray-100"
+        no-ring @click="isLinksOpen = !isLinksOpen"
+      >
+        <i-regular-bars v-show="!isLinksOpen" />
+        <i-regular-xmark v-show="isLinksOpen" />
+      </bh-button>
     </div>
+    <!-- 小屏幕下的链接区 -->
+    <transition name="link">
+      <div v-show="isLinksOpen" class="sm:hidden basis-full overflow-hidden">
+        <router-link
+          v-for="link in links" :key="link.path" :to="link.path"
+          class="block px-1 py-0.5 h-12 text-center group/link
+          text-gray-600 hover:text-gray-950 dark:text-gray-400 dark:hover:text-gray-100
+          fill-gray-600 hover:fill-gray-950 dark:fill-gray-400 dark:hover:fill-gray-100"
+          @click="isLinksOpen = false"
+        >
+          <div class="group-hover/link:bg-gray-200 dark:group-hover/link:bg-slate-800 p-1.5 rounded">
+            {{ link.name }}
+          </div>
+        </router-link>
+      </div>
+    </transition>
   </nav>
 </template>
+
+<style scoped>
+.link-enter-active, .link-leave-active {
+  transition: all 0.3s;
+}
+.link-enter-from, .link-leave-to {
+  height: 0;
+}
+.link-enter-to, .link-leave-from {
+  height: calc(v-bind(linkMenuHeight) * 3rem);
+}
+</style>
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -26,7 +26,7 @@ titleStore.title = t('routes.about')
     </h2>
     <img class="max-h-36 my-1" src="@/assets/images/avatar.png">
     <p class="my-1">
-      24岁，是学生，也是一名程序猿，主后端开发，前端也有所涉猎，甚至还是个半吊子美工。
+      已毕业，是一名程序猿，主后端开发，前端也有所涉猎，甚至还是个半吊子美工。
     </p>
     <p class="my-1">
       博主的学习动力基本上来源于视觉驱动和<a class="link" target="_blank" href="https://antfu.me/posts/about-yak-shaving-zh">Yak Shaving</a>，有时候看到一个不错的项目，可以是一个好看的页面设计，或者是一个实用的功能，就会尝试自己去复现出来，在这个过程可能会考虑到各种问题，比如性能、安全、用户体验等，又会去查阅相关资料，思考解决方案，通过这种Yak Shaving的方式，不知不觉就丰富了自己的技术栈。<span class="bg-black text-black hover:text-white">如果你通过链接去了解了Yak Shaving，这又何尝不是一种Yak Shaving呢？</span>
